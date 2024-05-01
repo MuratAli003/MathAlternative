7509e33ddc1e14f7a0a67159103ee103a1daa5f0
huawei <huawei@LAPTOP-2R5A6JR2>
2024-05-02 01:02:02 +0300

    Proje dosyası ekle.

---
+++ MathAlternatif/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MathAlternatif
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+       
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(330, 39);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(54, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Mutlak";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(166, 127);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "KareKök";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(54, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 31);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sin";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(166, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Cos";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(309, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 31);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Max";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(274, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "\"a,b şeklinde giriniz\"";
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(54, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Geri Al";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(34, 127);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 30);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "Mutlak";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(138, 127);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 30);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "Karekök";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(34, 184);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 30);
+            this.button9.TabIndex = 2;
+            this.button9.Text = "Sin";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button10.Location = new System.Drawing.Point(138, 184);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(80, 30);
+            this.button10.TabIndex = 3;
+            this.button10.Text = "Cos";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button11.Location = new System.Drawing.Point(288, 127);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(80, 30);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Max";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(34, 60);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(343, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button12.Location = new System.Drawing.Point(34, 238);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(80, 30);
+            this.button12.TabIndex = 6;
+            this.button12.Text = "Geri Al";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(255, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a b seklinde yaz";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(433, 333);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MathAlternatif/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MathAlternatif
+{
+
+    
+    public partial class Form1 : Form
+    {   
+        MathAlternative math = new MathAlternative();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string yazi_alan = "";
+        private void button7_Click(object sender, EventArgs e)
+        {
+            yazi_alan = textBox2.Text;
+            double num = Convert.ToDouble(textBox2.Text);
+            double sonuc = math.mutlakDeger_hesapla(num);
+            textBox2.Text = "+" + Convert.ToString(sonuc);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            yazi_alan = textBox2.Text;
+            double num = Convert.ToDouble(textBox2.Text);
+            if (num < 0)
+            {
+                MessageBox.Show("0 dan büyük sayı giriniz");
+            }
+            else {
+
+                double sonuc = math.sqrt(num);
+                textBox2.Text = Convert.ToString(sonuc);
+                
+
+            } 
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            yazi_alan = textBox2.Text;
+            string[] dizi = textBox2.Text.Split(' ');
+            double[] sayi_dizi = new double[dizi.Length];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                sayi_dizi[i] = Convert.ToDouble(dizi[i]);
+            }
+
+            double sonuc = math.max_hesapla(sayi_dizi);
+            textBox2.Text = Convert.ToString(sonuc);
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            yazi_alan = textBox2.Text;
+            double num = Convert.ToDouble(textBox2.Text)%360;
+            double aci = math.DereceToRadyan(num);
+            double sonuc = math.sin(aci);
+
+            textBox2.Text = Convert.ToString(sonuc);
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            yazi_alan = textBox2.Text;
+            double num = Convert.ToDouble(textBox2.Text)%360;
+            double aci = math.DereceToRadyan(num);
+            double sonuc = math.cos(aci);
+
+            textBox2.Text = Convert.ToString(sonuc);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = yazi_alan;
+        }
+    }
+    internal class MathAlternative
+    {
+        public MathAlternative() { }
+        public double sqrt(double sayi)
+        //   NEWTON - RAPHSON METHOD  \\ 
+        {
+            if (sayi > 0)
+            {
+                double yedek = sayi;
+                while (true)
+                {
+                    double kok = yedek - ((yedek * yedek) - sayi) / (2 * yedek);
+                    if (yedek - kok < 0.0000000001)
+                    {
+                        return kok; ;
+                    }
+                    yedek = kok;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sayı sıfırdan büyük olmalı");
+                return 0; 
+            }
+
+        }
+        public double mutlakDeger_hesapla(double sayi)
+        {
+            if (sayi < 0)
+            {
+                return -1 * sayi;
+            }
+            else
+            {
+                return sayi;
+            }
+
+        }
+        public double max_hesapla(params double[] sayi)
+        {
+            double max = sayi[0];
+            for (int i = 1; i < sayi.Length; i++)
+            {
+                if (max < sayi[i])
+                {
+                    max = sayi[i];
+                }
+            }
+            return max;
+        }
+
+        double power(double taban, int us)
+        {
+            double sonuc = 1;
+            for (int i = 0; i < us; i++)
+            {
+                sonuc *= taban;
+            }
+            return sonuc;
+        }
+        double factorial(int sayi)
+        {
+            double sonuc = 1;
+
+            for (int i = 1; i <= sayi; i++)
+            {
+                sonuc *= i;
+            }
+            return sonuc;
+        }
+        public double sin(double aci)
+        //  TAYLOR AND MACLAURİN SERİES  \\
+        {
+            double sonuc = 0;
+
+            if (aci == 180 * Math.PI / 180.0 || aci == 360 * Math.PI / 180.0 || aci == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    sonuc += (power(-1, i) * power(aci, (2 * i + 1)) / factorial(2 * i + 1));
+                }
+                return sonuc;
+            }
+        }
+        public double cos(double aci)
+        //  TAYLOR AND MACLAURİN SERİES  \\
+        {
+            double sonuc = 0;
+            if (aci == 90 * Math.PI / 180.0 || aci == 270 * Math.PI / 180.0)
+            {
+                return 0;
+            }
+            else
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    sonuc += (power(-1, i) * power(aci, (2 * i)) / factorial(2 * i));
+                }
+                return sonuc;
+            }
+        }
+        public double DereceToRadyan(double derece)
+        {
+            return derece * (Math.PI / 180.0);
+        }
+
+    }
+}
